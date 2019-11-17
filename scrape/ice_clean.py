86e9cf432d4bcf07429b7d9a0cf09ac4104f0dbe
Felix Dulys <felix.dulys@gmail.com>
2019-11-17 16:03:49 -0700

    map cleaning

--- scrape/ice_clean.py
+++ scrape/ice_clean.py
@@ -21,7 +21,29 @@ def clean_for_db(df, lat_key):
     return df
 
 
+def prep_for_map(path_to_scraped_data, path_to_geo_key, out_path):
+    df = (pd.read_csv(path_to_scraped_data)
+          .loc[lambda x: x.County != "All"]
+          .groupby(["County", "state"], as_index=False).sum()
+          .assign(state_abb=lambda x: x.state.apply(lambda y: STATES[y]),
+                  Area_name=lambda x: x.County.apply(lambda y: y.split(',')[0]))
+          )
+    df = df.assign(GEO=df.Area_name + ', ' + df.state)
+    key = df.loc[:, ["GEO", "Area_name", "state_abb", "state", "County"]].drop_duplicates()
+    ann = pd.read_csv(path_to_geo_key,
+                      usecols=["GEO.id", "GEO.display-label"],
+                      encoding="iso-8859-1")
+    ann = ann.rename(columns={"GEO.display-label": "GEO"})
+    key = pd.merge(ann, key, on="GEO", how="left")
+    for_map = df.loc[:, ["Arrests", "GEO"]]
+    for_map = for_map.loc[~for_map.Arrests.isna()]
+    for_map = pd.merge(key[["GEO", "GEO.id"]], for_map, on="GEO", how="left").fillna(0)
+    for_map = for_map.rename(columns={"GEO.id": "GEO_ID"})
+    for_map.to_csv(f"{out_path}map_data_ice_arrests.csv", index=False)
+
+
 def main():
+    # migrahack mvp
     arrests = pd.read_csv('month_year_co_counties.csv')
     lat_key = pd.read_csv('centroids.csv')
     new = clean_for_db(arrests, lat_key)
@@ -37,3 +59,9 @@ def main():
         print(row)
         json.dump(row, jsonfile)
         jsonfile.write(',\n')
+
+
+if __name__ == "__main__":
+    prep_for_map("~/dta/ice/all_states_ice_arrest.csv",
+                 "~/dta/geographic/locations/counties_GEOID_key.csv",
+                 "~/dta/ice/")
